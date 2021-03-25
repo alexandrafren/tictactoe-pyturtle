a200e93ebcb065fefd9f5a1f05ceee0abf70c2fa
alexandrafren <alex.fren@datadoghq.com>
2021-03-24 21:52:28 -0400

    initialize draw tokens, create Game class

--- ttt.py
+++ ttt.py
@@ -6,19 +6,71 @@ gm.bgcolor("lightpink")
 gm.setup(width=800, height=600)
 gm.tracer(0)
 
-def line(start, stop, width, len):
-    line = turtle.Turtle()
-    line.shape("square")
-    line.color("white")
-    line.penup()
-    line.goto(start, stop)
-    line.shapesize(stretch_wid=width, stretch_len=len)
+player_turn = "x"
 
 
-line(-100, 0, 25, .5)
-line(100, 0, 25, .5)
-line(0, 100, .5, 25)
-line(0, -100, .5, 25)
+class Game():
+
+    def __init__(self):
+        self.line(-100, 0, 25, .5)
+        self.line(100, 0, 25, .5)
+        self.line(0, 100, .5, 25)
+        self.line(0, -100, .5, 25)
+        self.title()
+        self.board = [" ", " ", " ",
+                      " ", " ", " ",
+                      " ", " ", " "]
+
+    def line(self, start, stop, width, len):
+        line = turtle.Turtle()
+        line.shape("square")
+        line.color("white")
+        line.penup()
+        line.goto(start, stop)
+        line.shapesize(stretch_wid=width, stretch_len=len)
+
+    def title(self):
+        pen = turtle.Turtle()
+        pen.speed(0)
+        pen.color("white")
+        pen.penup()
+        pen.hideturtle()
+        pen.goto(0, 260)
+        pen.write("Type Spot Number (1-9) to make your move", align="center", font=("Courier", 24, "normal"))
+
+
+def draw(token):
+    if token == "x":
+        t = turtle.Turtle()
+        t.goto(-50,-50)
+        t.goto(50,50)
+        t.hideturtle()
+        u = turtle.Turtle()
+        u.goto(50,-50)
+        u.goto(-50,50)
+        u.hideturtle()
+    else:
+        v = turtle.Turtle()
+        v.penup()
+        v.goto(-180, -250)
+        v.pendown()
+        v.circle(60)
+        v.hideturtle()
+
+def make_move(player_turn):
+    draw(player_turn)
+    if player_turn == "x":
+        player_turn = "o"
+    else:
+        player_turn = "x"
+
+
+
+
+
+Game()
+make_move("x")
+make_move("o")
 
 
 while True:
