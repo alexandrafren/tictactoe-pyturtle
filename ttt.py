3f8977057f37e7a9dd5b05997d479750a3981970
alexandrafren <alex.fren@datadoghq.com>
2021-03-27 20:10:59 -0400

    add all methods for onkeypress

--- ttt.py
+++ ttt.py
@@ -10,12 +10,15 @@ class Board:
         self.line(0, -100, .5, 25)
         self.title()
         self._board = [" ", " ", " ",
-                      " ", " ", " ",
-                      " ", " ", " "]
+                       " ", " ", " ",
+                       " ", " ", " "]
         self._current_player = "x"
 
     def get_board(self):
         return self._board
+
+    def set_board(self, index, player):
+        self._board[index] = player
 
     def get_current_player(self):
         return self._current_player
@@ -37,7 +40,7 @@ class Board:
         pen.color("white")
         pen.penup()
         pen.hideturtle()
-        pen.goto(0, 260)
+        pen.goto(0, 280)
         pen.write("Type Spot Number (1-9) to make your move", align="center", font=("Courier", 24, "normal"))
 
 
@@ -47,68 +50,208 @@ class Game:
         self._board = Board()
         self._state = "UNFINISHED"
 
-    def draw(self, token):
-        if token == "x":
-            t = turtle.Turtle()
-            t.goto(-50,-50)
-            t.goto(50,50)
-            t.hideturtle()
-            u = turtle.Turtle()
-            u.goto(50,-50)
-            u.goto(-50,50)
-            u.hideturtle()
-        else:
-            v = turtle.Turtle()
-            v.penup()
-            v.goto(-180, -250)
-            v.pendown()
-            v.circle(60)
-            v.hideturtle()
-
-    def make_move(self, key):
+    def game_state(self):
         if self._state != "UNFINISHED":
             return False
         else:
+            return True
+
+    def make_move1(self):
+        if self.game_state():
             player_turn = self._board.get_current_player()
-            self.draw(player_turn)
+            self._board.set_board(0, player_turn)
+            t = turtle.Turtle()
             if player_turn == "x":
+                t.shape("purplecross.gif")
                 self._board.set_current_player("o")
             else:
+                t.shape("purplecircle.gif")
                 self._board.set_current_player("x")
+            t.penup()
+            t.goto(-180, 180)
             self.game_won()
+        else:
+            return False
+
+    def make_move2(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(1, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(0, 180)
+            self.game_won()
+        else:
+            return False
+
+    def make_move3(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(2, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(180, 180)
+            self.game_won()
+        else:
+            return False
+
+    def make_move4(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(3, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(-180, 0)
+            self.game_won()
+        else:
+            return False
+
+    def make_move5(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(4, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(0, 0)
+            self.game_won()
+        else:
+            return False
+
+    def make_move6(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(5, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(180, 0)
+            self.game_won()
+        else:
+            return False
+
+    def make_move7(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(6, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(-180, -180)
+            self.game_won()
+        else:
+            return False
+
+    def make_move8(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(8, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(0, -180)
+            self.game_won()
+        else:
+            return False
+
+    def make_move9(self):
+        if self.game_state():
+            player_turn = self._board.get_current_player()
+            self._board.set_board(8, player_turn)
+            t = turtle.Turtle()
+            if player_turn == "x":
+                t.shape("purplecross.gif")
+                self._board.set_current_player("o")
+            else:
+                t.shape("purplecircle.gif")
+                self._board.set_current_player("x")
+            t.penup()
+            t.goto(180, -180)
+            self.game_won()
+        else:
+            return False
 
     def game_won(self):
         board = self._board.get_board()
+        print(board)
         wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8],
                 [0, 3, 6], [1, 4, 7], [2, 5, 8],
                 [0, 4, 8], [2, 4, 6]]
         for s in wins:
             if board[s[0]] == board[s[1]] and board[s[1]] == board[s[2]] and board[s[0]] != " ":
                 self._state = board[s[0]]
+                pw = turtle.Turtle()
+                pw.speed(0)
+                pw.color("white")
+                pw.penup()
+                pw.hideturtle()
+                pw.goto(0, -280)
+                pw.write("Player {} won!".format(board[s[0]]), align="center", font=("Courier", 24, "normal"))
 
 
 
 def main():
     gm = turtle.Screen()
     gm.title("Tic Tac Toe")
-    gm.bgcolor("lightpink")
-    gm.setup(width=800, height=600)
+    gm.bgcolor("pink")
+    gm.setup(width=800, height=650)
     gm.tracer(0)
+    gm.register_shape("purplecross.gif")
+    gm.register_shape("purplecircle.gif")
     g = Game()
+    gm.onkeyrelease(g.make_move1, 1)
+    gm.onkeyrelease(g.make_move2, 2)
+    gm.onkeyrelease(g.make_move3, 3)
+    gm.onkeyrelease(g.make_move4, 4)
+    gm.onkeyrelease(g.make_move5, 5)
+    gm.onkeyrelease(g.make_move6, 6)
+    gm.onkeyrelease(g.make_move7, 7)
+    gm.onkeyrelease(g.make_move8, 8)
+    gm.onkeyrelease(g.make_move9, 9)
     gm.listen()
-    gm.onkeypress(g.make_move(1), "1")
-    gm.onkeypress(g.make_move(2), "2")
-    gm.onkeypress(g.make_move(3), "3")
-    gm.onkeypress(g.make_move(4), "4")
-    gm.onkeypress(g.make_move(5), "5")
-    gm.onkeypress(g.make_move(6), "6")
-    gm.onkeypress(g.make_move(7), "7")
-    gm.onkeypress(g.make_move(8), "8")
-    gm.onkeypress(g.make_move(9), "9")
-    g.game_won()
+
     while True:
         gm.update()
 
-
 if __name__ == "__main__":
     main()
+
+
